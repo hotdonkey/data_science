359344079527bb077ef39a0cc69bc8a05cdaa986
hotdonkey <nayorash@gmail.com>
2023-08-22 07:05:06 +0500

    server

--- PROD/server.py
+++ PROD/server.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hello')
-def hello():
-    return "hello world"
+def hello_funk():
+    return "hello"
 
 if __name__ == '__main__':
     app.run('localhost', 5000)
