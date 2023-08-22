b2ec27f85ee3a229f79a662306e5b77342fed999
hotdonkey <nayorash@gmail.com>
2023-08-22 14:15:55 +0500

    prod

--- PROD/server.py
+++ PROD/server.py
@@ -1,16 +1,55 @@
-from flask import Flask, request
+import numpy as np
+import pandas as pd
+import pickle
+
+
+from flask import Flask, request, jsonify
+import datetime
+
+
+with open("/Users/kirillkuznecov/Documents/Документы/data_science/PROD/data/model.pkl", 'rb') as pkl_model:
+    model = pickle.load(pkl_model)
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_funk():
-    return "hello"
 
 @app.route('/')
 def index():
     return "Test message. The server is running"
 
+
+@app.route("/add", methods=["POST"])
+def add():
+    num = request.json.get("num")
+    if num > 10:
+        return 'too much', 400
+
+    return jsonify({
+        'result': num + 1
+    })
+
+
+@app.route("/predict", methods=["POST"])
+def prediction():
+
+    features = request.json
+    features = np.array(features).reshape(1, 4)
+
+    pred = model.predict(features)
+
+    return jsonify({"prediction": pred[0]})
+
+
+@app.route("/time")
+def current_time():
+    return {'time': datetime.datetime.now()}
+
+
+@app.route('/hello')
+def hello_funk():
+    name = request.args.get("name")
+    return f"hello {name}"
+
+
 if __name__ == '__main__':
     app.run('localhost', 8000)
-    
-    
