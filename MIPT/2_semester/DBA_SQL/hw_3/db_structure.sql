20b2e8aa54fb2712826b5668a8e59da83a6353ef
hot_donkey <nayorash@gmail.com>
2025-02-20 22:40:11 +0500

    db_hw_3

---
+++ MIPT/2_semester/DBA_SQL/hw_3/db_structure.sql
@@ -0,0 +1,37 @@
+create table if not exists customer (
+customer_id int4
+, first_name varchar(50)
+, last_name varchar(50)
+, gender varchar (30)
+, dob varchar(50)
+, job_title varchar(50)
+, job_industry_category varchar(50)
+, wealth_segment varchar(50)
+, deceased_indicator varchar(50)
+, owns_car varchar(30)
+, address varchar(50)
+, postcode varchar(30)
+, state varchar(30)
+, country varchar(30)
+, property_valuation int4
+);
+
+create table if not exists transaction (
+transaction_id int4
+, product_id int4
+, customer_id int4
+, transaction_date varchar(30)
+, online_order varchar(30)
+, order_status varchar(30)
+, brand varchar(30)
+, product_line varchar(30)
+, product_class varchar(30)
+, product_size varchar(30)
+, list_price float4
+, standard_cost float4
+);
+
+-- Изменим тип данных для удобства дальнейших запросов
+
+ALTER TABLE public.transaction 
+ALTER COLUMN transaction_date TYPE date USING to_date(transaction_date, 'DD.MM.YYYY');
